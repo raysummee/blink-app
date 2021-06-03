c324b1b3c35baba11e5d798eff354c95aec4870f
Angshuman Barpujari <angshuarin@gmail.com>
2021-06-03 17:46:40 +0530

    added card button

---
+++ lib/components/buttons/cardButton.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CardButton extends StatelessWidget {
+  const CardButton({ required this.label, required this.icon, required this.func, Key? key }): super(key: key);
+
+  final String label;
+  final IconData icon;
+  final VoidCallback func;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      child: InkWell(
+        onTap: func,
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16),
+          height: 60,
+          child: Row(
+            children: [
+              Icon(icon),
+              SizedBox(width: 8,),
+              Text(
+                label
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
