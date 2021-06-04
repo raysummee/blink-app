cdd6e674f6ee22520eac78ae1dcaf258b5795a1d
Angshuman Barpujari <angshuarin@gmail.com>
2021-06-04 16:43:01 +0530

    extracted homepage from the main file

---
+++ lib/screens/homePage.dart
@@ -0,0 +1,37 @@
+import 'package:blink_app/components/buttons/cardButton.dart';
+import 'package:blink_app/components/cards/ConnectCard.dart';
+import 'package:blink_app/components/texts/GuideTexts.dart';
+import 'package:blink_app/logic/helper/HelperBlinkScan.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Blink"
+        ),
+      ),
+      body: Column(
+        children: [
+          ConnectCard(),
+          CardButton(
+            label: "Settings",
+            icon: Icons.settings,
+            func: (){},
+          ),
+          CardButton(
+            label: HelperBlinkScan().readAll().toString(),
+            icon: Icons.gamepad_outlined,
+            func: (){},
+          ),
+          SizedBox(height:8,),
+          GuideTexts(),
+        ],
+      ),
+    );
+  }
+}
