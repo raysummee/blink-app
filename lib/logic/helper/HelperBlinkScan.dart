6a780eaab0c2fe8653ba759971893bdcda550cdf
Angshuman Barpujari <angshuarin@gmail.com>
2021-06-04 17:24:24 +0530

    helper of blink scan updated

--- lib/logic/helper/HelperBlinkScan.dart
+++ lib/logic/helper/HelperBlinkScan.dart
@@ -8,4 +8,18 @@ class HelperBlinkScan{
   List<ModelBlinkScan> readAll(){
     return Db.store!.box<ModelBlinkScan>().getAll();
   }
+
+  void deleteAll(){
+    Db.store!.box<ModelBlinkScan>().removeAll();
+  }
+
+  //if id == 0, add new, else will update existing
+  void putOne(ModelBlinkScan blinkScan){
+    Db.store!.box<ModelBlinkScan>().put(blinkScan);
+  }
+
+  //only will tigger but stream will not have any value
+  Stream listen(){
+    return Db.store!.watch<ModelBlinkScan>();
+  }
 }
